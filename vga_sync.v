c40d5239c9e2c12d73c6abadb05bcb03167befa6
Alexandre Dumont <adumont@gmail.com>
2018-05-05 01:08:19 +0200

    Add ifndef SYNTHESIS to support simulation

--- vga_sync.v
+++ vga_sync.v
@@ -51,20 +51,27 @@ module vga_sync (
     //
 	// Pixel clock.
 
-    SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
-                    .PLLOUT_SELECT("GENCLK"),
-                    .DIVR(4'b0000),
-                    .DIVF(7'b1010011),
-                    .DIVQ(3'b101),
-                    .FILTER_RANGE(3'b001)
-            )
-            uut
-            (
-                    .REFERENCECLK(clk),
-                    .PLLOUTCORE(px_clk),
-                    .RESETB(1'b1),
-                    .BYPASS(1'b0)
-              );
+
+    `ifndef SYNTHESIS
+    // SIMULATION
+        assign px_clk = clk;
+    `else
+    // SYNTHESIS
+        SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
+                        .PLLOUT_SELECT("GENCLK"),
+                        .DIVR(4'b0000),
+                        .DIVF(7'b1010011),
+                        .DIVQ(3'b101),
+                        .FILTER_RANGE(3'b001)
+                )
+                uut
+                (
+                        .REFERENCECLK(clk),
+                        .PLLOUTCORE(px_clk),
+                        .RESETB(1'b1),
+                        .BYPASS(1'b0)
+                    );
+    `endif
 
     /*
     http://www.epanorama.net/faq/vga2rgb/calc.html
