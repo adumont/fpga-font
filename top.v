29948c2fcbba5cbb91577a1cfd987ea30fb69bd9
Alexandre Dumont <adumont@gmail.com>
2020-04-09 00:23:07 +0200

    remove unneeded blocks in top, for counter

--- top.v
+++ top.v
@@ -36,7 +36,7 @@ module top (
     /* out */ .px_clk(px_clk)             // Pixel clock
     );
 
-    `define Zoom 2
+    `define Zoom 1
 
     // STAGE 1
 
@@ -48,33 +48,20 @@ module top (
     reg hsync2, vsync2, activevideo2;
     reg hsync3, vsync3, activevideo3;
 
-    wire [25:0] RGBStr0;
-    reg  [25:0] RGBStr1, RGBStr2, RGBStr3;
+    // wire [25:0] RGBStr0;
+    // reg  [25:0] RGBStr1, RGBStr2, RGBStr3;
 
-    assign RGBStr0 = { 3'b000, px_x0, px_y0, hsync0, vsync0, activevideo0 };
+    // assign RGBStr0 = { 3'b000, px_x0, px_y0, hsync0, vsync0, activevideo0 };
 
     always @( posedge px_clk) begin
       { hsync1, vsync1, activevideo1, px_x1, px_y1 } <= { hsync0, vsync0, activevideo0, px_x0, px_y0 };
       { hsync2, vsync2, activevideo2, px_x2, px_y2 } <= { hsync1, vsync1, activevideo1, px_x1, px_y1 };
       { hsync3, vsync3, activevideo3, px_x3, px_y3 } <= { hsync2, vsync2, activevideo2, px_x2, px_y2 };
       
-      RGBStr1 <= RGBStr0;
-      RGBStr2 <= RGBStr1;
-      RGBStr3 <= RGBStr2;
+      // RGBStr1 <= RGBStr0;
+      // RGBStr2 <= RGBStr1;
+      // RGBStr3 <= RGBStr2;
     end
-
-    /* TODO tilemem0
-        - change addressing scheme to optime for low BRAM resources on the iCE40hx-1k
-    */    
-
-    // ouput wires
-    wire [`FONT_WIDTH-1:0] tilemem0_char_code;
-
-    tilemem #( .ZOOM( `Zoom ) ) tilemem0 (
-    /*  in */ .clk( px_clk ),
-    /*  in */ .RGBStr_i( RGBStr0 ),
-    /* out */ .char_code( tilemem0_char_code )
-    );
 
     reg [`FONT_WIDTH-1:0] char_shown;
 
@@ -115,22 +102,22 @@ module top (
     /* out */  .data( font_bit )         // Output RGB stream.
     );
 
-    // TODO: Embed in a combination block
-    // takes input: stream 3
+    // TODO: Embed in a combbin_to_ascii2ck
+    // takes input: stream 3bin_to_ascii2
     // TODO: place register at the end to sync... (stream4)
 
     always @(*) begin
-        rgb <= 3'b000;
+        rgb = 3'b000;
         if (activevideo3) begin
             if ( font_bit )
-                rgb <= 3'b010;
+                rgb = 3'b010;
             else if (px_y3 == 0 || px_y3 == 479 || px_x3 == 0 || px_x3 == 639 )
-                rgb <= 3'b001;
+                rgb = 3'b001;
             else
-                rgb <= 3'b000;
+                rgb = 3'b000;
         end
         else
-            rgb <= 3'b000;
+            rgb = 3'b000;
     end
 
     assign hsync = hsync3;
